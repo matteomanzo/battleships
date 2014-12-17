95950147fe7d875077daa902c331a4ae79554c7d
Emily Sas <emily.bronwen.sas@gmail.com>
2014-12-17 19:34:01 +0000

    attempt to place whole ship length, not yet working

--- lib/board.rb
+++ lib/board.rb
@@ -18,13 +18,37 @@ class Board
 
   def new_cell_assignment(cell, value)
     cell_content = self.grid[cell]
-    raise "There is already a ship in this cell" if cell_content.is_a?(Ship) unless value == :hit
+    #raise "There is already a ship in this cell" if cell_content.is_a?(Ship) unless value == :hit
     self.grid[cell] = value
   end
 
   def ship_hit(cell, ship)
     new_cell_assignment(cell, :hit)
     ship.hit!
+  end
+
+  def place_ship(cell, ship, orientation)
+    size = ship.size
+    cell = cell.to_s
+    cell_array = cell.split(//)
+    l = cell_array[0]
+    n = cell_array[1]
+    if orientation == "horizontal"
+      size.times do
+        n = n.to_i
+        next_n = n+1;
+        new_cell = "#{@l}#{next_n}".to_sym
+        new_cell_assignment(new_cell, ship)
+      end
+    # elsif orientation == "vertical"
+    #   size.times do
+    #     next_l = ??????;
+    #     new_cell = next_l+n
+    #     new_cell_assignment(new_cell, ship)
+    #   end
+    # else 
+    #   raise "please state either a horizontal or vertical orientation"
+    end  
   end
 
 end
